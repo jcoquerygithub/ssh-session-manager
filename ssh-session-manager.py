93036e3f115c40ddbfaf5d554f6a8eeb621b5f8a
jcoquerygithub <32167067+jcoquerygithub@users.noreply.github.com>
2018-08-07 16:35:33 +0200

    Added launch of putty app including tree path as title Window

--- ssh-session-manager.py
+++ ssh-session-manager.py
@@ -1,7 +1,11 @@
+import os
+import subprocess
+
 import gi
+import xml.etree.ElementTree as Et
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-import xml.etree.ElementTree as ET
+
 
 class Handler:
     def onDestroy(self, *args):
@@ -14,7 +18,10 @@ class Handler:
         model, treeiter = selection.get_selected()
         if treeiter is not None:
             server_view = builder.get_object("server_tree_view")
-            server_view.expand_row(selection.get_selected_rows()[1][0], False)
+            if server_view.row_expanded(selection.get_selected_rows()[1][0]):
+                server_view.collapse_row(selection.get_selected_rows()[1][0])
+            else:
+                server_view.expand_row(selection.get_selected_rows()[1][0], False)
             print("You selected", model[treeiter][0])
 
     def on_row_activated(self, view, path, column):
@@ -22,25 +29,33 @@ class Handler:
         treeiter = model.get_iter(path)
         if treeiter is not None:
             print("You db click on", model[treeiter][0])
+            subprocess.Popen(['/usr/bin/putty', '-title', get_path_unix_style(model, treeiter), model[treeiter][1]])
 
 
 def add_to_store(store, parent, node):
-    store_node = store.append(parent, [node.get('Name')])
+    store_node = store.append(parent, [node.get('Name'), node.get('Hostname')])
     if len(node.getchildren()) != 0:
         for subnodes in node:
             add_to_store(store, store_node, subnodes)
 
+def get_path_unix_style(model, node):
+    parent = model.iter_parent(node)
+    if parent is None:
+        return model[node][0]
+    else:
+        return '{}/{}'.format(get_path_unix_style(model, parent),model[node][0])
 
 builder = Gtk.Builder()
 builder.add_from_file("ssh-session-manager.glade")
 
 #: :type: Gtk.TreeStore
 server_store = builder.get_object("server_store")
-tree = ET.parse('../../../progs/mremoteng/confCons.xml')
+if os.path.isfile('../../../progs/mremoteng/confCons.xml'):
+    tree = Et.parse('../../../progs/mremoteng/confCons.xml')
 
-#: :type: ET.ElementTree
-root = tree.getroot()
-add_to_store(server_store, None, root)
+    #: :type: Et.ElementTree
+    root = tree.getroot()
+    add_to_store(server_store, None, root)
 
 builder.connect_signals(Handler())
 
